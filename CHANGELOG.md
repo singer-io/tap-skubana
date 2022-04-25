b0c830dffc49c74519d1d00a2bf93b4ff6e50444
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2022-04-25 14:07:48 +0000

    Version 0.0.3 and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 0.0.3
+  * Share requests Session across streams and retry immediately up to 5 times on `requests.ConnectionError` exceptions [#6](https://github.com/singer-io/tap-skubana/pull/6)
+
 ## 0.0.2
   * Update rate limit backoff calc
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-skubana',
-      version='0.0.2',
+      version='0.0.3',
       description='Singer.io tap for extracting data from the Skubana API',
       author='scott.coleman@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
